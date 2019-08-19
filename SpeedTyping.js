22ffd0718b8bf5420ad602a03ed73bab5b4c4050
Hasantha malshan <43041192+hasanthamalshan@users.noreply.github.com>
2019-08-19 12:09:04 +0530

    add ui

---
+++ SpeedTyping.js
@@ -0,0 +1,10 @@
+let time = 5 ;
+let score = 0 ;
+let isPlaying;
+
+const wordInput = document.querySelector('');
+const wordInput = document.querySelector('');
+const wordInput = document.querySelector('');
+const wordInput = document.querySelector('');
+const wordInput = document.querySelector('');
+const wordInput = document.querySelector('');
