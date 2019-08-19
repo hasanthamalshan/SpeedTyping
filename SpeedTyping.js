c92515a988720847316a05567611ea9128807fe1
Hasantha malshan <43041192+hasanthamalshan@users.noreply.github.com>
2019-08-19 14:41:07 +0530

    title animation

--- SpeedTyping.js
+++ SpeedTyping.js
@@ -1,10 +1,24 @@
-let time = 5 ;
-let score = 0 ;
-let isPlaying;
+var i = 0;
+var j = 10;
+var text = "type me ...";
+var time = 10;
 
-const wordInput = document.querySelector('');
-const wordInput = document.querySelector('');
-const wordInput = document.querySelector('');
-const wordInput = document.querySelector('');
-const wordInput = document.querySelector('');
-const wordInput = document.querySelector('');
+function typing() {
+    if (i < text.length) {
+        document.getElementById("title").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typing, 300);
+    }
+}
+
+function timecount() {
+    if (j >= 0) {
+        if (j == 10) {
+            document.getElementById("time").innerHTML = "00:" + j;
+        } else {
+            document.getElementById("time").innerHTML = "00:0" + j;
+        }
+        j--;
+        setTimeout(timecount, 1000);
+    }
+}
