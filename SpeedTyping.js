95c0d4e406f1d8a93283566ea91fc4198e6d7f3c
Hasantha malshan <hasanthamalshan07@gmail.com>
2019-10-30 02:19:36 +0530

    add js

--- SpeedTyping.js
+++ SpeedTyping.js
@@ -3,6 +3,19 @@ var j = 10;
 var text = "type me ...";
 var time = 20;
 
+let letters = [];
+const words = ['apple', 'orange', 'grapes', 'mango'];
+
+function displayWord() {
+
+    let word = words[0];
+    document.getElementById("text").innerHTML += word;
+
+    letters = word.split('');
+    document.getElementById("text1").innerHTML += letters;
+
+}
+
 function typing() {
     if (i < text.length) {
         document.getElementById("title").innerHTML += text.charAt(i);
@@ -22,3 +35,7 @@ function timecount() {
         setTimeout(timecount, 1000);
     }
 }
+
+typing()
+displayWord()
+timecount()
